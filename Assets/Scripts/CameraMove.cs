a795e7fb62182aee412800574ce394e2bb56cc5e
Aidan <aidan.t.park@gmail.com>
2024-07-18 17:29:41 -0400

    Reworked Camera move code to be more reliable

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -6,33 +6,31 @@ using UnityEngine.Rendering;
 
 public class CameraMove : MonoBehaviour
 {
-    private int edgeScrollSize = Screen.width / 50;
-    [SerializeField] private float rotateSpeed = 0.40f;
-    private const float rotationLimit = 45;
+    [SerializeField] private int ScreenScrollBorderSize;
+    [SerializeField] private float RotateSpeed = 60.0f;
+    private int EdgeScrollSize;
+    private bool turning = false;
     
     [SerializeField] private Texture2D handRight;
     [SerializeField] private Texture2D handLeft;
     [SerializeField] private Texture2D handDefault;
-    private bool turning = false;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        EdgeScrollSize = Screen.width / ScreenScrollBorderSize;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float rotateDir = 0f;
-        if (Input.mousePosition.x < edgeScrollSize) rotateDir = -1f;
-        if (Input.mousePosition.x > Screen.width - edgeScrollSize) rotateDir = +1f;
+        if (Input.mousePosition.x < EdgeScrollSize || Input.GetKey(KeyCode.A)) rotateDir = -1f;
+        if (Input.mousePosition.x > Screen.width - EdgeScrollSize || Input.GetKey(KeyCode.D)) rotateDir = +1f;
         
         //Rotate stop points
-        var yRotation = transform.eulerAngles.y;
+        //var yRotation = transform.eulerAngles.y;
         
-        if (yRotation is >= rotationLimit and < 180) rotateDir = Math.Min(0, rotateDir);
-        if (yRotation is <= 360 - rotationLimit and > 180) rotateDir = Math.Max(0, rotateDir);
+        //if (yRotation is >= rotationLimit and < 180) rotateDir = Math.Min(0, rotateDir);
+        //if (yRotation is <= 360 - rotationLimit and > 180) rotateDir = Math.Max(0, rotateDir);
         
         //Manage cursor icon
         if (rotateDir > 0)
@@ -50,6 +48,6 @@ public class CameraMove : MonoBehaviour
             Cursor.SetCursor( handDefault, new Vector2(0,0), CursorMode.Auto);
         }
 
-        transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed, 0);
+        transform.eulerAngles += new Vector3(0, rotateDir * RotateSpeed * Time.deltaTime, 0);
     }
 }
