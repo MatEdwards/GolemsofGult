070b2a403d440d349928f5bdaaba6400ffc3d0e4
MatEdwards <matt.ellisedwards@gmail.com>
2024-07-18 09:34:47 -0400

    Adding cursor for draggable / screenmove
    TODO: Use cursor manager instead to avoid glitchy cursor when doing multiple actions

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -9,6 +9,11 @@ public class CameraMove : MonoBehaviour
     private int edgeScrollSize = Screen.width / 50;
     [SerializeField] private float rotateSpeed = 0.40f;
     private const float rotationLimit = 45;
+    
+    [SerializeField] private Texture2D handRight;
+    [SerializeField] private Texture2D handLeft;
+    [SerializeField] private Texture2D handDefault;
+    private bool turning = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +33,22 @@ public class CameraMove : MonoBehaviour
         
         if (yRotation is >= rotationLimit and < 180) rotateDir = Math.Min(0, rotateDir);
         if (yRotation is <= 360 - rotationLimit and > 180) rotateDir = Math.Max(0, rotateDir);
+        
+        //Manage cursor icon
+        if (rotateDir > 0)
+        {
+            Cursor.SetCursor( handRight, new Vector2(handRight.width / 2,handRight.height / 2), CursorMode.Auto);
+            turning = true;
+        } else if (rotateDir < 0)
+        {
+            Cursor.SetCursor( handLeft, new Vector2(handLeft.width / 2,handLeft.height / 2), CursorMode.Auto);
+            turning = true;
+        }
+        else if (turning)
+        {
+            turning = false;
+            Cursor.SetCursor( handDefault, new Vector2(0,0), CursorMode.Auto);
+        }
 
         transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed, 0);
     }
--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -5,12 +5,17 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
 
 public class Draggable : MonoBehaviour
 {
     Rigidbody rb;
     private Vector3 startingPos; 
     private float depth = 4;
+
+    [SerializeField] private Texture2D handClosed;
+    [SerializeField] private Texture2D handOver;
+    [SerializeField] private Texture2D handDefault;
     
     // Start is called before the first frame update
     void Start()
@@ -18,24 +23,29 @@ public class Draggable : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    private void OnMouseDown()
+    private void OnMouseOver()
     {
-        //Vector3 startingPos = transform.position;
+        Cursor.SetCursor( handOver, new Vector2(handOver.width / 2,handOver.height / 2), CursorMode.Auto);
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor( handDefault, new Vector2(0,0), CursorMode.Auto);
     }
 
     private void OnMouseDrag()
     {
-
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,depth);
-
-        Vector3 mouseDirection = Camera.main.ScreenToWorldPoint(mousePosition);
         
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,depth);
+        Vector3 mouseDirection = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = mouseDirection;
         
+        Cursor.SetCursor( handClosed, new Vector2(handClosed.width / 2,handClosed.height / 2), CursorMode.Auto);
         rb.isKinematic = true;
     }
     private void OnMouseUp()
     {
+        Cursor.SetCursor( handDefault, new Vector2(0,0), CursorMode.Auto);
         rb.isKinematic = false;
     }
     
